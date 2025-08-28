6a78e5dc6bed108eafb5da95dddb36224bfd2016
Schmid Michael Peter <michaelpeter.schmid17@gmail.com>
2025-08-28 09:51:12 +0200

    rebuilded project

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // if this is a *project* page:
-  base: '/.',
+  base: './',
 
   // (alternative that also works for project pages)
   // base: './'
